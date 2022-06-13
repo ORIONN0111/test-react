3f3b26ff2491c39ec1e0e9ce9e64749082df7628
ORIONN0111 <poppinpoppin111@gmail.com>
2022-06-14 07:50:12 +0900

    画像表示

--- src/App.js
+++ src/App.js
@@ -1,19 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
-import pic from "images/Image.png"
+import pic from "./images/Image.png";
 
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//       </header>
-//     </div>
-//   );
-// }
-
-export const ImageTest {
-  return <img src={pic} alt="picture" />
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={pic} alt="map" />
+      </header>
+    </div>
+  );
 }
 
 export default App;
